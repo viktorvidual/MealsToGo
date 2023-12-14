307fe7be5e1d3a056a32ddde33237c8ddf36735f
viktorvidual <viktor.petrov@vidual.io>
2023-12-14 14:32:15 +0200

    Adding Theme

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-
 import { RestaurantsScreen } from "./src/features/restauraunts/screens/RestaurantScreen";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
